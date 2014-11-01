ae252e400892ba4779f8bbe47fde7054b27b891b
Aaron Panchal <aaron.panchal@appfolio.com>
2014-11-01 00:19:02 -0700

    Move CSS to separate file, display velocity vector

--- freccia/touch_path.js
+++ freccia/touch_path.js
@@ -27,6 +27,43 @@
         return this.points[this.points.length - 1];
       },
 
+      net: function() {
+        var start = this.start(),
+            end   = this.end();
+
+        return { x: end.x - start.x, y: end.y - start.y };
+      },
+
+      time: function() {
+        var start = this.start(),
+            end   = this.end();
+
+        return (end.moment - start.moment) / 1000;
+      },
+
+      netDistance: function() {
+        var net = this.net();
+        return Math.sqrt(Math.pow(net.x, 2) + Math.pow(net.y, 2));
+      },
+
+      netSpeed: function() {
+        return this.netDistance() / this.time();
+      },
+
+      unit: function() {
+        var net = this.net(),
+            distance = this.netDistance();
+
+        return { x: net.x / distance, y: net.y / distance };
+      },
+
+      velocity: function() {
+        var unit = this.unit(),
+            speed = this.netSpeed();
+        
+        return { x: unit.x * speed, y: unit.y * speed };
+      },
+
       addPoint: function(touchPoint) {
         return this.points.push(touchPoint);
       }
